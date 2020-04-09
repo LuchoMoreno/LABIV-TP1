a86f86b0dd86dfd876d2519c6c42d588cbe09ce3
luchomoreno <lucho.moreno@live.com>
2020-04-09 11:16:25 -0300

    Modificacion completa de archivos para hacer funcionar en ZEIT

---
+++ src/app/componentes/carga-mascota/carga-mascota.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit} from '@angular/core';
+
+import { Mascota } from 'src/app/Clases/Mascota/mascota';
+
+
+
+// 1- Crear un componente 'carga-mascota' que nos permita cargar nombre, imagen (con inputs) y 
+// tipo (con select) y que devuelva por medio de un output los datos cargados.
+
+
+@Component({
+  selector: 'app-carga-mascota',
+  templateUrl: './carga-mascota.component.html',
+  styleUrls: ['./carga-mascota.component.css']
+})
+export class CargaMascotaComponent implements OnInit {
+
+
+  // CREO LOS ATRIBUTOS DE LA MASCOTA.
+
+  nombre : string = null;
+
+  imagen : string = null;
+
+  tipo : string = null;
+
+
+  // CREO UN ARRAY DE LISTA DE MASCOTAS.
+  lista : Array<Mascota> = [];
+
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.nombre = "";
+  }
+
+
+  public enviarAListado (event): void {
+        
+    this.lista.push(new Mascota(this.nombre, this.imagen, this.tipo));
+    console.log(this.lista);
+  }
+
+
+}
